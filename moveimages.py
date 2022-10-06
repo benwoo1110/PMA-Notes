0ac5a709e799f692e8a63a75cdd91ba8ae3a94cc
Ben Woo <30431861+benwoo1110@users.noreply.github.com>
2022-10-06 21:31:27 +0800

    Add Lab 15-02

---
+++ moveimages.py
@@ -0,0 +1,31 @@
+import re
+from pathlib import Path
+
+filename = input("Enter the filename: ")
+filepath = Path(filename)
+
+if (not filepath.is_file()):
+    print("File does not exist")
+    exit(1)
+
+with open(filepath, 'r') as file:
+    data = file.read()
+
+imagematches = re.findall(r'!\[.*\]\((.*)\)', data)
+folder = filepath.parent
+
+for i, imagename in enumerate(imagematches, 1):
+    imagefile = Path(folder, imagename)
+    if (not imagefile.is_file()):
+        print("Image file does not exist")
+        continue
+
+    newimagefile = Path(folder, "Images", f"{filepath.stem}-{i:02d}.png")
+    
+    imagefile.rename(newimagefile)
+    data.replace(imagename, f"../Images/{newimagefile.name}")
+
+with open(filepath, 'w') as file:
+    file.write(filepath)
+
+print("Done!")
